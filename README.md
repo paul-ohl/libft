6d4c9760fedcb27fa05c30fe269049e570df29b0
Paul OHL <paul.lv.ohl@gmail.com>
2022-08-29 11:21:10 -0400

    removed useless file and added README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Libft
+
+This is the first project we have to do at school 42.
+
+Every file is a helper function that will be used later in other projects.
--- libft.h
+++ libft.h
@@ -20,12 +20,6 @@
 
 # include <strings.h>
 
-typedef struct	s_list
-{
-	void			*content;
-	struct s_list	*next;
-}				t_list;
-
 int				ft_isalnum(int c);
 int				ft_isalpha(int c);
 int				ft_isascii(int c);
@@ -39,7 +33,6 @@ char			*ft_strrchr(const char *str, char c);
 int				ft_tolower(int c);
 int				ft_toupper(int c);
 int				ft_atoi(const char *str);
-int				ft_atoi_increment(const char *str, int *i);
 void			*ft_memset(void *str, int c, size_t n);
 void			ft_bzero(void *str, size_t n);
 void			*ft_memcpy(void *dst, const void *src, size_t len);
@@ -61,15 +54,6 @@ void			ft_putstr_fd(char *s, int fd);
 void			ft_putchar_fd(char c, int fd);
 void			ft_putendl_fd(char *s, int fd);
 void			ft_putnbr_fd(int n, int fd);
-t_list			*ft_lstnew(void *content);
-void			ft_lstadd_front(t_list **alst, t_list *new);
-int				ft_lstsize(t_list *lst);
-t_list			*ft_lstlast(t_list *lst);
-void			ft_lstadd_back(t_list **alst, t_list *new);
-void			ft_lstdelone(t_list *lst, void (*del)(void*));
-void			ft_lstclear(t_list **lst, void (*del)(void*));
-void			ft_lstiter(t_list *lst, void (*f)(void *));
-t_list			*ft_lstmap(t_list *lst, void *(*f)(void *), void (*d)(void *));
 int				get_next_line(int fd, char **line);
 int				is_whitespace(char c);
 int				ft_intlen(int nbr);
